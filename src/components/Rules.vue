bcfe0e3fb6ab7e1d6af9ce3281700cd67adc0e42
DinnerWithoutTomato <xqiutongxue@foxmail.com>
2022-08-28 15:27:52 +0800

    fix: 补充计算规则

--- src/components/Rules.vue
+++ src/components/Rules.vue
@@ -6,10 +6,7 @@ const [showRules, toggleShowRules] = useToggle(false)
 
 <template>
   <div
-    m="l-1"
-    s-4
-    i-ri:question-fill
-    cursor-pointer
+    ml-1 s-4 i-ri:question-fill cursor-pointer
     @click="() => toggleShowRules()"
   />
   <NModal v-model:show="showRules">
@@ -27,17 +24,21 @@ const [showRules, toggleShowRules] = useToggle(false)
         <li>每天送单抽 + 第一天送 10 抽，共 14 + 10 = 24 抽</li>
         <li>额外高难关卡 5 ~ 9 关，按 5 源石算</li>
       </ul>
+      <li>故事集：商店 3 抽 + 新关卡约 16 源石</li>
+      <li>新主线：按第十章 38 源石计算</li>
       <li>搓玉：按每天 200 合成玉算，不计算额外体力</li>
-      <li>月卡：200 合成玉</li>
+      <li>月卡：200 合成玉（6 源石不计入统计）</li>
       <li>绿票商店：默认全部换满，一层 2 抽 + 600 合成玉 | 二层 2 抽</li>
       <li>剿灭任务：默认打满，1800合成玉/周 | 新剿灭额外 1500 合成玉</li>
       <li>签到：每个月第 17 天签到送 1 抽</li>
-      <li>故事集：是否有合成玉 or 抽</li>
       <li>黄票商店：换满 38 抽，仅统计一次</li>
+      <li>直播奖励：周年、半周年直播奖励约 1200 合成玉</li>
       <br>
-      <span>暂时没统计的</span>
-      <li>大月卡：每月 10 抽</li>
+      <span>暂未统计</span>
+      <li>商店中小月卡之外的所有组合包（包括大月卡）</li>
+      <li>悖论模拟：每关 200 合成玉</li>
       <li>邮件送的：太复杂了，不考虑</li>
+      <li>小活动奖励（猜不透自由的鹰角）</li>
     </n-card>
   </NModal>
 </template>
